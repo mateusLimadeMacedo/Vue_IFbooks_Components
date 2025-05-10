e18105aee7b014d1cf924b3b1080abd35c5f6cc6
erickChavesFiamoncini <erickechaves@gmail.com>
2025-05-10 16:52:37 -0300

    criação da home #3

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,84 @@
 <script setup>
+import { reactive, ref } from 'vue';
+
+
+const booleano = ref(false);
+
+const produtos = reactive([
+  {
+    id: 1,
+    titulo: 'Chain of Iron: Volume 2',
+    autor: 'Cassandra Clare',
+    preco: 23.24,
+    quantidade: 1,
+    capa: 'public/imgs/chain of torn.jpeg',
+    precoSoma: 23.24,
+  },
+  {
+    id: 2,
+    titulo: 'Chain of Thorn: Volume 2',
+    autor: 'Cassandra Clare',
+    preco: 23.24,
+    quantidade: 1,
+    capa: 'public/imgs/chai of torn vdd.jpeg',
+    precoSoma: 23.24,
+  },
+  {
+    id: 3,
+    titulo: 'City of Fallen Angels',
+    autor: 'Cassandra Clare',
+    preco: 13.94,
+    quantidade: 1,
+    capa: 'public/imgs/city of.jpeg',
+    precoSoma: 13.94,
+  },
+  {
+    id: 4,
+    titulo: 'Nona the Ninth',
+    autor: 'Cassandra Clare',
+    preco: 16.84,
+    quantidade: 1,
+    capa: 'public/imgs/clock work.jpeg',
+    precoSoma: 16.84,
+  },
+  {
+    id: 5,
+    titulo: 'Harlem Shuffle',
+    autor: 'Colson Whitehead',
+    preco: 26.92,
+    quantidade: 1,
+    capa: 'public/imgs/parece um quadrino.jpeg',
+    precoSoma: 26.92,
+  },
+  {
+    id: 6,
+    titulo: 'Two Old Women',
+    autor: 'Velma Wallis',
+    preco: 13.95,
+    quantidade: 1,
+    capa: 'public/imgs/women.jpeg',
+    precoSoma: 13.95,
+  },
+  {
+    id: 7,
+    titulo: 'Carrie Soto Is Back',
+    autor: 'Taylor Jenkins Reid',
+    preco: 26.04,
+    quantidade: 1,
+    capa: 'public/imgs/carrie soto.jpeg',
+    precoSoma: 26.04,
+  },
+  {
+    id: 8,
+    titulo: 'Book Lovers',
+    autor: 'Emily Henry',
+    preco: 15.81,
+    quantidade: 1,
+    capa: 'public/imgs/book lovers.png',
+    precoSoma: 15.81,
+  }
+]);
+
 
 </script>
 
@@ -51,7 +131,59 @@
   </section>
 
 
+  <div v-if="booleano === false" id="dois">
+    <div class="book-description">
+      <h3>Autor de Abril</h3>
+      <h1>Eric-Emanuel Schmitt</h1>
+      <p>
+        Eric-Emmanuel Schmitt has been awarded more than 20 literary prizes and distinctions, and in
+        2001 he received the title of Chevalier des Arts et des Lettres. His books have been
+        translated into over 40 languages.
+      </p>
+      <button>Acessar página do livro</button>
+    </div>
+    <div class="img">
+      <div>
+        <img src="/public/imgs/9ca5b51d70fa54350c0c18670357cf85.png" alt="Eric-Emanuel Schmitt book" />
+      </div>
+      <div class="text-p">
+        <p>*within the stock limit</p>
+      </div>
+    </div>
+  </div>
+  <div v-if="booleano === false" class="states">
+    <div>
+      <i class="fa-solid fa-box"></i>
+      <p>frete grátis para SC</p>
+    </div>
+    <div id="state-middle">
+      <i class="fa-solid fa-star"></i>
+      <p>Livros recomendados</p>
+    </div>
+    <div class="line">
+      <i class="fa-solid fa-book-open"></i>
+      <p>Mais vendidos</p>
+    </div>
+  </div>
+  <div v-if="booleano === false" class="books">
+    <h1>Lançamentos</h1>
+    <div class="books-container">
 
+      <div class="shop" v-for="produto in produtos" :key="produto.id">
+        <img :src="produto.capa" alt="">
+        <h3>{{ produto.titulo }}</h3>
+        <p>{{ produto.autor }}</p>
+        <div class="like">
+          <p> {{ produto.preco.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }) }}</p>
+          <button class="coracao" @click="favoritar"><i class="fa-solid fa-heart"></i></button>
+        </div>
+
+        <button @click="adicionar(produto)" class="comprar"><i class="fa-solid fa-cart-shopping"></i> Comprar</button>
+
+      </div>
+    </div>
+
+  </div>
 
 
    <footer>
@@ -191,6 +323,199 @@ div ul {
 .search i{
   color: #231F2D;
   cursor: pointer;
+}
+#dois {
+  display: flex;
+  padding: 6vw 0vw 4vw 12vw;
+  justify-content: center;
+  align-items: center;
+}
+
+.book-description {
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  width: 70%;
+}
+
+#dois h3 {
+  border: #27ae60 solid 0.1rem;
+  border-radius: 0.3vw;
+  padding: 0.5vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #27ae60;
+  width: 20%;
+  margin-bottom: 1.5vw;
+}
+#dois h1 {
+  font-size: 3.5rem;
+  font-weight: bold;
+  color: #382c2c;
+}
+
+.book-description p {
+  width: 45%;
+  font-size: 1.1rem;
+  margin-top: 1.1vw;
+}
+
+#dois button {
+  margin-top: 2.7vw;
+  width: 30%;
+  font-size: 1.1rem;
+  padding: 0.85vw 1vw;
+  background-color: #27ae60;
+  color: white;
+  border: none;
+  border-radius: 4px;
+}
+
+.img {
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+
+}
+
+.img img {
+  width: 65%;
+  height: 50%;
+}
+
+.text-p {
+  display: flex;
+  align-items: end;
+  justify-content: end;
+  width: 70%;
+
+}
+
+.states {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  gap: 20px;
+  padding: 100px;
+  border-bottom: 1px #27ae60 solid;
+  border-top: #27ae60 solid 1px;
+}
+
+.states div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  padding: 20px;
+}
+
+.states p,
+.states i {
+  font-size: 30px;
+  font-weight: bold;
+  color: #382c2c;
+}
+
+.states .line p {
+  border-bottom: #382c2c solid 1px;
+  padding-right: 2vw;
+}
+
+#state-middle {
+  border-left: 1px solid;
+  border-right: 1px solid;
+  padding: 20px;
+  width: 650px;
+}
+
+.books {
+  padding: 100px;
+  display: flex;
+  gap: 20px;
+  flex-direction: column;
+}
+
+.books h1 {
+  font-size: 3rem;
+  font-weight: bold;
+  margin: 2vw 0 3.5vw 2.5vw;
+  color: #382c2c;
+}
+
+.books-container {
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 6vw;
+  flex-wrap: wrap;
+  gap: 10px;
+}
+
+.books-container .shop {
+  width: 22%;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 4vw;
+}
+
+.books-container .shop img {
+  width: 100%;
+  border-radius: 3px;
+}
+
+.coracao {
+  color: #27ae60;
+  cursor: pointer;
+}
+
+.books-container .shop h3 {
+  margin-top: 1vw;
+  font-size: 1.6rem;
+  color: #382c2c;
+  font-weight: bold;
+
+}
+
+.books-container .shop p {
+  color: #4f4c57;
+  margin-bottom: 0.6vw;
+  font-size: 1.1rem;
+}
+
+.books-container .shop div p {
+  font-size: 1.3rem;
+  font-weight: bold;
+  color: #382c2c;
+}
+
+.books-container .shop .comprar {
+  border: none;
+  background-color: #27ae60;
+  color: white;
+  padding: 0.8vw 1vw;
+  border-radius: 3px;
+  font-size: 1rem;
+  margin-top: 1.3vw;
+  cursor: pointer;
+}
+
+.books-container .shop img {
+  height: 100%;
+}
+
+.books-container .shop .like {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  text-align: center;
+}
+
+.books-container .shop .like button {
+  background-color: white;
+  color: #27ae60;
+  border: none;
+  padding: 0 1vw 0 0;
+  font-size: 1.6rem;
 }
 /*=================================================================================Footer==============================================================*/
 
