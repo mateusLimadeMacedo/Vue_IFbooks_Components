13b289cc8f3c3fb19d4fb87fbd7619b9e98650bb
erickChavesFiamoncini <erickechaves@gmail.com>
2025-05-10 17:18:38 -0300

    atualizacao cores

--- src/App.vue
+++ src/App.vue
@@ -79,7 +79,45 @@ const produtos = reactive([
   }
 ]);
 
+const carrinho = reactive({
+  items: [
 
+  ],
+
+  frete: 0,
+  desconto: 0,
+  total: 0,
+});
+
+function adicionar(produto) {
+  let jaExiste = carrinho.items.find(item => item.id == produto.id);
+  if (!jaExiste) {
+    carrinho.items.push({...produto});
+    carrinho.total += produto.preco
+  }
+  else {
+    alert(`O livro já foi adicionado ao carrinho.`);
+  }
+}
+function incrementar(item) {
+  item.quantidade++
+  item.precoSoma += item.preco
+  carrinho.total += item.preco
+}
+
+function decrementar(item) {
+  if (item.quantidade > 1) {
+    item.quantidade--
+    item.precoSoma -= item.preco
+    carrinho.total -= item.preco
+  }
+  else {
+    const pos = carrinho.items.findIndex(i => i.id == item.id);;
+    carrinho.items.splice(pos, 1);
+    carrinho.total -= item.preco;
+
+  }
+}
 </script>
 
 <template>
@@ -185,6 +223,73 @@ const produtos = reactive([
 
   </div>
 
+   <div class="booleano" v-if="(booleano === true)">
+    <h3><Span>Carrinho</Span></h3>
+    <div class="atributos">
+      <p>Título</p>
+      <p>Quantidade</p>
+      <p>Subtotal</p>
+    </div>
+
+    <div class="itens" v-for="item in carrinho.items" :key="item.id">
+      <div class="dados">
+        <img :src="item.capa" alt="">
+        <div class="coluna">
+          <h3> {{ item.titulo }} </h3>
+          <p id="autor"> {{ item.autor }} </p>
+          <p id="preco"> {{ item.preco.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }) }}</p>
+        </div>
+      </div>
+      <div class="quantidade">
+        <button @click="decrementar(item)">-</button>
+        <p>{{ item.quantidade }}</p>
+        <button v-on:click="incrementar(item)">+</button>
+      </div>
+      <div class="preco_geral">
+        <p>{{ item.precoSoma.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }) }}</p>
+      </div>
+
+    </div>
+
+    <button @click="booleano = !booleano" class="voltar">Voltar para a loja</button>
+
+    <div class="abaixo">
+
+      <div class="cupom">
+        <input type="text" placeholder="Código do cupom" />
+        <button >Inserir Cupom</button>
+      </div>
+
+      <div class="total_compraT">
+        <div class="total_compra">
+        <h3>Total da Compra</h3>
+
+        <div class="produtos">
+          <p>Produtos:</p>
+          <p>{{ carrinho.total.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }) }}</p>
+        </div>
+
+        <div class="frete">
+          <p>Frete:</p>
+          <p>Grátis</p>
+        </div>
+
+        <div class="total">
+          <p>Total:</p>
+          <p>{{ carrinho.total.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }) }}</p>
+        </div>
+      </div>
+        <div id="button">
+
+          <button>Ir para o pagamento</button>
+
+        </div>
+
+
+      </div>
+
+    </div>
+  </div>
 
    <footer>
     <div class="maior">
@@ -390,6 +495,7 @@ div ul {
   align-items: end;
   justify-content: end;
   width: 70%;
+  color: black;
 
 }
 
@@ -611,5 +717,220 @@ footer .maior {
   width: 57px;
   border-radius: 4px;
 }
+/*==============================================================================Carrinho=================================================================*/
+.booleano {
+  padding: 3vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 0 5vw;
+}
 
+.booleano h3 span {
+  color: #27ae60;
+  font-size: 2.5rem;
+  font-weight: 500;
+
+
+}
+
+.booleano .atributos {
+  display: flex;
+  justify-content: space-between;
+  margin: 5vw 0 0 0;
+  font-size: 1.4rem;
+  padding-bottom: 1vw;
+  border-bottom: #27ae60 solid 1px;
+  color: black;
+  font-weight: 600;
+}
+
+.dados, .quantidade, .preco_geral{
+  width: 33%;
+
+}
+
+.preco_geral{
+  display: flex;
+  justify-content: end;
+}
+
+.itens {
+  padding: 3vw 1vw;
+  display: flex;
+  border-bottom: #b6b5b5 solid 2px;
+  align-items: center;
+  justify-content: space-between;
+
+}
+
+.itens .dados img {
+  width: 150px;
+  height: 220px;
+  border-radius: 3px;
+  margin-right: 3vw;
+}
+
+.itens .coluna {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.itens .coluna h3 {
+  font-weight: 900;
+  font-size: 1.5rem;
+  max-height: 40px;
+  max-width: 25vw;
+  word-wrap: break-word;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+  color: #382C2C;
+
+
+}
+
+.itens .coluna #autor {
+  margin: 0.8vw 0 0.8vw 0;
+  font-size: 1.1rem;
+  color: #4F4C57;
+}
+
+.itens .coluna #preco {
+  font-weight: 700;
+  font-size: 1.5rem;
+  color: #382C2C;
+}
+
+.itens .quantidade {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: #000000 solid 1px;
+  height: 56px;
+  width: 124px;
+}
+
+.itens .quantidade button {
+  border: none;
+  background-color: white;
+  font-size: 1.5rem;
+}
+
+.itens .quantidade p {
+  font-size: 1.5rem;
+  padding: 0 10px;
+  color: black;
+}
+
+.preco_geral p {
+  font-size: 1.6rem;
+  font-weight: 600;
+  color: #382c2c;
+}
+.booleano .voltar{
+  margin: 3vw 0 6vw;
+  width: 15%;
+  padding: 0.9vw 1.3vw;
+  border: #000000 solid 1px;
+  font-size: 1.1rem;
+  background-color: white;
+  border-radius: 4px;
+  font-weight: 500;
+}
+.cupom input{
+  font-size: 1.1rem;
+  padding: 0.2vw 1.6vw;
+  background-color: rgb(255, 255, 255);
+  border: black solid 1px;
+  outline: 0;
+  border-radius: 4px;
+  margin-right: 1vw;
+  height: 16%;
+}
+
+.cupom input::placeholder{
+  color: rgb(136, 136, 136);
+  font-size: 1.1rem;
+}
+.cupom{
+  display: flex;
+
+}
+.cupom button{
+  font-size: 1.1rem;
+  padding: 0.2vw 3vw;
+  background-color: #27ae60;
+  border-radius: 4px;
+  margin-right: 1vw;
+  height: 16%;
+  color: white;
+  border: none;
+
+}
+.abaixo{
+  display: flex;
+  justify-content: space-between;
+
+}
+.total_compra {
+  display: flex;
+  flex-direction: column;
+  padding: 1.5vw 1.5vw 0;
+  width: 430px;
+  justify-content: center;
+  color: black;
+
+
+
+}
+.total_compraT {
+  display: flex;
+  flex-direction: column;
+  border: #000000 solid 1px;
+  padding: 1vw;
+  justify-content: center;
+  align-items: center;
+  border-radius: 4px;
+
+
+}
+.total_compra h3{
+  font-size: 1.5rem;
+  margin-bottom: 1vw;
+
+}
+.produtos,
+.frete,
+.total{
+  display: flex;
+  justify-content: space-between;
+  border-bottom: rgb(185, 185, 185) solid 1px;
+  padding: 0.8vw 0vw;
+
+}
+.total{
+  border: none;
+}
+.total_compraT button{
+  font-size: 1.1rem;
+  background-color: #27ae60;
+  border-radius: 4px;
+  margin-right: 1vw;
+  color: white;
+  border: none;
+  text-align: center;
+  padding: 17px 60px;
+
+
+}
+#button{
+  text-align: center;
+  align-items: center;
+}
+
+button {
+  cursor: pointer;
+}
 </style>
